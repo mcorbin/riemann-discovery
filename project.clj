f00ee7936e6b56d4b80fa001a50b48d356e6c300
mcorbin <corbin.math@gmail.com>
2017-06-13 00:01:59 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann-discovery "0.1.0"
+(defproject riemann-discovery "0.1.1-SNAPSHOT"
   :description "Service discovery for Riemann (riemann.io)"
   :url "https://github.com/mcorbin/riemann-discovery"
   :license {:name "MIT"}
